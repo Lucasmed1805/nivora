ded1b32f6d567ae609dfe60671c031d4da9ef391
Lucasmed1805 <lucas.med1805@gmail.com>
2026-04-04 11:06:29 -0300

    atualização do site

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,25 +1,29 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import HowItWorks from "@/components/HowItWorks";
+import Exemplos from "@/components/Exemplos";
+import ProvaSocial from "@/components/ProvaSocial";
 import UseCases from "@/components/UseCases";
 import Benefits from "@/components/Benefits";
 import Pricing from "@/components/Pricing";
 import CTA from "@/components/CTA";
 import Footer from "@/components/Footer";
+import WhatsAppButton from "@/components/WhatsAppButton";
 
-const Index = () => {
+export default function Index() {
   return (
     <div className="min-h-screen bg-background">
       <Navbar />
       <Hero />
       <HowItWorks />
+      <Exemplos />
+      <ProvaSocial />
       <UseCases />
       <Benefits />
       <Pricing />
       <CTA />
       <Footer />
+      <WhatsAppButton />
     </div>
   );
-};
-
-export default Index;
+}
